eac98605f99c70a71aa42b84b80391b35899a5e9
Ruel Misa <ruel.g.misa@gmail.com>
2019-11-18 00:09:58 -0500

    Not so initial commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: MemberCard(),
+));
+
+class MemberCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Member Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/images/workout-1.jpg'
+                ),
+                radius: 80.0,
+                
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[650],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.5,
+
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Alicia Crawford',
+              style: TextStyle(
+                color: Colors.pinkAccent[200],
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0
+              )
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'MEMBER SINCE',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.5,
+
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '11.17.2019',
+              style: TextStyle(
+                color: Colors.pinkAccent[200],
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0
+              )
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.phone,
+                  size: 15.0,
+                  color: Colors.grey[350],
+                ),
+                SizedBox(width: 5.0),
+                Text(
+                  '363.445.7212',
+                  style: TextStyle(
+                    color: Colors.grey[350],
+                    fontSize: 20.0,
+                    letterSpacing: 2.0
+                  )
+                )
+              ],
+            )
+          ],
+        )
+      )
+    );
+  }
+}
